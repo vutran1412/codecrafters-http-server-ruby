43fe819eadbe4cd6789363c892996d99038f8908
Vu Tran <vutran1412@icloud.com>
2024-12-09 14:29:48 -0600

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -2,4 +2,10 @@ require "socket"
 
 server = TCPServer.new("localhost", 4221)
 client_socket, client_address = server.accept
-client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+line = client_socket.gets
+target = line.split(' ')[1]
+if target == "/"
+  client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+else
+  client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+end
