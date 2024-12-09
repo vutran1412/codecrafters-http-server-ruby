375899829b106590a2971063bafb41f76821d0aa
Vu Tran <vutran1412@icloud.com>
2024-12-09 13:49:47 -0600

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -2,5 +2,4 @@ require "socket"
 
 server = TCPServer.new("localhost", 4221)
 client_socket, client_address = server.accept
-
-puts "HTTP/1.1 200 OK\r\n\r\n"
+client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
