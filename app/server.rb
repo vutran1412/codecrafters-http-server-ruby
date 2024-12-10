40c48f79c7146a1d88d8d95cf778e7e7d925aa25
Vu Tran <vutran1412@icloud.com>
2024-12-10 10:49:40 -0600

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -1,15 +1,39 @@
 require "socket"
 
 server = TCPServer.new("localhost", 4221)
-client_socket, client_address = server.accept
-line = client_socket.gets
-puts line.inspect
-target = line.split(' ')[1]
-if target == "/"
-  client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
-elsif target.include?("/echo/")
-  response_body = target.split("/").last
-  client_socket.puts "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{response_body.bytesize}\r\n\r\n#{response_body}"
-else
-  client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+puts "Server running on port 4221"
+loop do
+  client_socket, client_address = server.accept
+  puts "Client connected"
+  headers = []
+  while (line = client_socket.gets)
+    break if line.strip.empty?
+    headers << line
+  end
+  puts "Request Headers:\n#{headers.join("\n")}"
+  request_line = headers.first
+  if request_line
+    method, path, _http_version = request_line.split(" ")
+    if path == "/"
+      client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+    elsif path == "/user-agent"
+      user_agent = headers.find { |header| header.start_with?("User-Agent:") }
+      if user_agent
+        user_agent = user_agent.split(": ", 2)[1]
+        response_body = user_agent.strip
+        client_socket.puts "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{response_body.bytesize}\r\n\r\n#{response_body}"
+      else
+        client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+      end
+    elsif path.start_with?("/echo/")
+      response_body = path.split("/echo/").last
+      client_socket.puts "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: #{response_body.bytesize}\r\n\r\n#{response_body}"
+    else
+      client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+    end
+  else
+    client_socket.puts "HTTP/1.1 400 Bad Requst\r\n\r\n"
+  end
+  client_socket.close
+  puts "Client disconnected"
 end
